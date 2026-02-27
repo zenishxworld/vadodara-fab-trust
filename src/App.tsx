0d307c8eaf588793a17d073fda3658209488fbb5
Zenish Patel <zenishpatel2004@gmail.com>
2026-02-27 18:11:27 +0530

    fix: remove base path for Netlify deployment, add _redirects for SPA routing

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/vadodara-fab-trust">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
